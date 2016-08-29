dfca1590de4835d9f707bb3f88b78970d8791e88
Chris Parker <mrcsparker@gmail.com>
2016-08-29 00:12:12 -0500

    Adding vagrant setup

--- setup.sh
+++ setup.sh
@@ -1,2 +1,23 @@
 #!/usr/bin/env bash
 
+sudo apt-get update
+
+sudo apt-get install -y git
+sudo apt-get install -y git-core
+
+# Docker setup
+
+sudo apt-get install apt-transport-https ca-certificates
+sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+sudo touch /etc/apt/sources.list.d/docker.list
+echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" | sudo tee -a /etc/apt/sources.list.d/docker.list
+
+sudo apt-get update
+
+sudo apt-get install linux-image-extra-$(uname -r) linux-image-extra-virtual
+
+sudo apt-get install docker-engine
+
+sudo service docker start
+
+sudo systemctl enable docker
